e695435d12b0afc348f18400b31b25bae52643fc
esveritas <esveritas@gmail.com>
2013-10-15 14:41:21 +0200

    Update DetectLineImage.py
    Optimized (Thnx Dennis ;))

--- DetectLineImage.py
+++ DetectLineImage.py
@@ -11,6 +11,7 @@ class DetectLineImage():
         height, width = imgTemp.size
         img = cv2.imread(imgString)
         self.threshold = threshold
+        self.thresholdSquared = threshold*threshold
         self.start(width, height, img)
         
     def start(self, width, height, img):    
@@ -19,14 +20,14 @@ class DetectLineImage():
         for x in xrange(0, width):
             for y in xrange(0, height):
                 pixel = img[x][y]
-                if(self.accepted(pixel[0], pixel[1], pixel[2])):
+                if(self.accepted(pixel[2], pixel[1], pixel[0])):
                     pixels[y, x] = (255,255,255)
     
     #Eucledian distance of white and pixel we're reading
     def accepted(self, X, Y, Z):
     #white = [255, 255, 255]
-        D = math.sqrt((255-X)*(255-X) + (255-Y)*(255-Y) + (255-Z)*(255-Z))
-        if D <= self.threshold:
+        D = (255-X)*(255-X) + (255-Y)*(255-Y) + (0-Z)*(0-Z)
+        if D <= self.thresholdSquared:
             return True
         return False
      
@@ -39,7 +40,7 @@ class DetectLineImage():
 #End of class
 
 #Example code to illustrate this class' use
-dli = DetectLineImage('Nao_Image2.png', 200)
+dli = DetectLineImage('Balloon.png', 125)
 img = dli.getNewImage()
 threshold = dli.getThreshold()
 plt.title("Threshold = " + str(threshold))
