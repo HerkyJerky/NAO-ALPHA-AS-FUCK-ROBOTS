6a29d767c2e36c4b44bc5aca69726c32b25c23ca
Dennis Soemers <d.soemers@gmail.com>
2013-11-10 22:13:26 +0100

    Split up OldActualSlam into AbstractSLAMProblem and GraphSLAM. Renamed some stuff, (imo) cleaned some stuff up.. hope you still like it taghi :P
    Seems to work fine, with one exception: I changed things so that it is no longer necessary for the robot to have seen every single landmark in a simulation (I saw in your code that if the robot missed a landmark, you'd just start everything over again). When that happens now though, it still crashes. Did not delete OldActualSlam yet so it can be used for comparison

--- Nao Robots/SLAM/GraphSLAM.py
+++ Nao Robots/SLAM/GraphSLAM.py
@@ -4,68 +4,19 @@ Created on 7 nov. 2013
 @author: Taghi
 @author: Dennis
 '''
+     
+from AbstractSLAMProblem import *;
+import numpy
 
-import numpy as np
-import random
-import math as MH
-
-class actualSlam:
-    
-    def __init__(self, world_size = 100.0, measurement_range = 30.0,
-                 motion_noise = 1.0, measurement_noise = 1.0):
-        self.measurement_noise = 0.0
-        self.world_size = world_size
-        self.measurement_range = measurement_range
-        self.x = world_size / 2.0
-        self.y = world_size / 2.0
-        self.motion_noise = motion_noise
-        self.measurement_noise = measurement_noise
-        self.landmarks = []
-        self.num_landmarks = 0
-        
-    def make_landmarks(self, num_landmarks):
-        self.landmarks = []
-        for i in range(num_landmarks):
-            self.landmarks.append([round(random.random() * self.world_size),
-                                   round(random.random() * self.world_size)])
-        self.num_landmarks = num_landmarks
-        
-        
-    def move(self, dx, dy):
-
-        x = self.x + dx + self.rand() * self.motion_noise
-        y = self.y + dy + self.rand() * self.motion_noise
-
-        if x < 0.0 or x > self.world_size or y < 0.0 or y > self.world_size:
-            return False
-        else:
-            self.x = x
-            self.y = y
-            return True
-    
-
-    def sense(self):
-        Z = []
-        for i in range(self.num_landmarks):
-            dx = self.landmarks[i][0] - self.x + self.rand() * self.measurement_noise
-            dy = self.landmarks[i][1] - self.y + self.rand() * self.measurement_noise    
-            if self.measurement_range < 0.0 or abs(dx) + abs(dy) <= self.measurement_range:
-                Z.append([i, dx, dy])
-        return Z
-    
-    def rand(self):
-        return random.random() * 2.0 - 1.0
-
-def slam(data,N,num_landmarks, motion_noise, measurement_noise, initialX, initialY):
+def graphSlam(data, N, num_landmarks, motion_noise, measurement_noise, initialX, initialY):
     dim = 2*(N + num_landmarks)
     
-    
-    Omega = np.zeros((dim,dim))
+    Omega = numpy.zeros((dim,dim))
     #Omega = np.zeros((dim,dim))
     Omega[0,0] = 1.0
     Omega[1,1] = 1.0
     
-    Xi = np.zeros((dim,1))
+    Xi = numpy.zeros((dim,1))
     Xi[0,0] = initialX
     Xi[1,0] = initialY
     
@@ -97,92 +48,39 @@ def slam(data,N,num_landmarks, motion_noise, measurement_noise, initialX, initia
  
     #print np.size(Omega, 0)
     #print np.size(Omega, 1)
-    invOmega = np.linalg.inv(Omega)
+    invOmega = numpy.linalg.inv(Omega)
     
     #invOmega = np.linalg.cholesky(Omega)
     #Omega = np.linalg.pinv(invOmega)
-    mu = np.dot(invOmega,Xi)
-    
+    mu = numpy.dot(invOmega,Xi)
     
     return mu
-    #return 1   
-       
-def make_data(N, num_landmarks, world_size, measurement_range, motion_noise, 
-              measurement_noise, distance):
 
-    complete = False
-
-    while not complete:
-
-        data = []
-
-        # make robot and landmarks
-        r = actualSlam(world_size, measurement_range, motion_noise, measurement_noise)
-        r.make_landmarks(num_landmarks)
-        seen = [False for row in range(num_landmarks)]
-    
-        # guess an initial motion
-        orientation = random.random() * 2.0 * MH.pi
-        dx = MH.cos(orientation) * distance
-        dy = MH.sin(orientation) * distance
-    
-        for k in range(N-1):
-    
-            # sense
-            Z = r.sense()
-
-            # check off all landmarks that were observed 
-            for i in range(len(Z)):
-                seen[Z[i][0]] = True
-    
-            # move
-            while not r.move(dx, dy):
-                # if we'd be leaving the robot world, pick instead a new direction
-                orientation = random.random() * 2.0 * MH.pi
-                dx = MH.cos(orientation) * distance
-                dy = MH.sin(orientation) * distance
-
-            # memorize data
-            data.append([Z, [dx, dy]])
-
-        # we are done when all landmarks were observed; otherwise re-run
-        complete = (sum(seen) == num_landmarks)
-
-    print ' '
-    print 'Landmarks: ', r.landmarks
-    print r
-
-    return data       
-  
-  
-def print_result(N, num_landmarks, result):
+def print_result(num_steps, num_landmarks, result):
     print
     print 'Estimated Pose(s):'
-    for i in range(N):
+    for i in range(num_steps):
         print '    ['+ ', '.join('%.3f'%x for x in result[2*i]) + ', ' \
             + ', '.join('%.3f'%x for x in result[2*i+1]) +']'
     print 
     print 'Estimated Landmarks:'
     for i in range(num_landmarks):
-        print '    ['+ ', '.join('%.3f'%x for x in result[2*(N+i)]) + ', ' \
-            + ', '.join('%.3f'%x for x in result[2*(N+i)+1]) +']'
-                   
+        print '    ['+ ', '.join('%.3f'%x for x in result[2*(num_steps+i)]) + ', ' \
+            + ', '.join('%.3f'%x for x in result[2*(num_steps+i)+1]) +']'
+  
 '''
 This is the test case. I will just assume some numbers to check if it actually works
-'''        
+''' 
 if __name__ == "__main__":
-    import sys
-    N = 2
+    numSteps = 3
     world_size = 100.0
     measurement_range = 50.0
-    distance = 3.0
-    num_landmarks = 0
+    walkingDistancePerStep = 3.0
+    num_landmarks = 2
     measurement_noise = 1.0
     motion_noise = 1.0
-    data = make_data(N,num_landmarks,world_size,measurement_range,motion_noise,measurement_noise,distance)
-    a = slam(data,N,num_landmarks,motion_noise,measurement_noise, 50.0, 50.0)
-    print_result(N,num_landmarks, a)
     
-    
-    
-    
+    problem = AbstractSLAMProblem(world_size, measurement_range, motion_noise, measurement_noise, num_landmarks);
+    data = problem.run_simulation(numSteps, num_landmarks, world_size, measurement_range, motion_noise, measurement_noise, walkingDistancePerStep);
+    mu = graphSlam(data, numSteps, num_landmarks, motion_noise, measurement_noise, 50.0, 50.0)
+    print_result(numSteps,num_landmarks, mu)
