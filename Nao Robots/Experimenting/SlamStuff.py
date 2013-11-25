aa57ec53812026c4f1ebf121a5052412359cee6e
Dennis Soemers <d.soemers@gmail.com>
2013-11-25 12:39:06 +0100

    Gabby requests, SwaggyP delivers

--- Nao Robots/Experimenting/SlamStuff.py
+++ Nao Robots/Experimenting/SlamStuff.py
@@ -12,5 +12,17 @@ class SlamStuff:
 
     def slam(self):
         # TODO Dennis and Taghi, fill in some beautiful code. penis.
+        
+        '''
+        As requested by Gabi:
+          ___
+         //  7
+        (_,_/\
+         \    \
+          \    \
+          _\    \__
+         (   \     )
+          \___\___/  
+        '''
         logObj.logWrite(time.time().__str__() + "_7_0_0_0_0")
         pass
