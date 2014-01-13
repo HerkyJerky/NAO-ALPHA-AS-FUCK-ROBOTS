1555cf05b66bda89f2e40d4960ee211f4ad18430
esveritas <esveritas@gmail.com>
2014-01-13 22:36:43 +0100

    Almost ready and stuff

--- Nao Robots/ImageProcessing/ImageProcessing.py
+++ Nao Robots/ImageProcessing/ImageProcessing.py
@@ -11,7 +11,9 @@ class ImageProcessing():
         imgTemp = Image.open(imgString)
         self.height, self.width = imgTemp.size
         self.img = cv2.imread(imgString)
-        self.maxSpacing = 0.0;
+        self.maxSpacing = 10;
+        self.LOWER_BOUND_GREEN = 0.0
+        self.UPPER_BOUND_GREEN = 0.85
                
                 
     # width of image, height of image, image, fraction of the image to be randomly taken pixels from            
@@ -45,26 +47,25 @@ class ImageProcessing():
     def startLumi(self):    
         self.new_img = Image.new('RGB', (self.height, self.width), "black")
         #pixels = self.img.load()
+      
         
         for x in xrange(0, self.width):
             for y in xrange(0, self.height):
                 B, G, R = self.img[x][y]
-                #Using REC. 601 algorithm to calculate the grayscale of RGB value
-                Y = 0.2126 * R + 0.7152 * G + 0.0722 * B
-                
-                #Getting the Hue
+               
+                #Getting the Hue and Lum
                 R = np.double(R)/255
                 G = np.double(G)/255
                 B = np.double(B)/255
                 H = csc.rgb_to_hue(R, G, B)
+                Y = csc.rgb_to_luminance(R, G, B)
                 
+               
                 #accepted white
-                if(self.acceptedLumi(Y)):
+                if(self.acceptedLumi(Y*255)):
                     self.img[x, y] = (255,255,255)
                 elif(self.acceptedGreen(H)):
                     self.img[x, y] = (0,255,0)
-                #elif(self.acceptedBlack(Y)):
-                #    pixels[x, y] = (0,255,0)
                 else:
                     self.img[x, y] = (0, 0, 0)
                     
@@ -73,6 +74,7 @@ class ImageProcessing():
 
     def removeBackGround(self):
         #self.white_list = []
+        
         w = self.getHeightImage()
         h = self.getWidthImage()
         for x in xrange(0, w):
@@ -80,7 +82,7 @@ class ImageProcessing():
             
             for y in xrange(0, h):
                 B, G, R = self.img[y][x]
-                if(B == 0 and G == 255 and R == 0):
+                if(B == 0 and G == 255):
                     count = count + 1
                 else:
                     count = 0
@@ -88,19 +90,22 @@ class ImageProcessing():
                     for r in xrange(0, y-count+1):
                         self.img[r, x] = (0, 0, 0)
                     break
+            if(count <= self.maxSpacing):
+                for y in xrange(0, h):
+                    self.img[y, x] = (0, 0, 0)
                 
         #self.clusterImage()
-        #self.onlyWhite()
-        self.abstractImage()
+        self.onlyWhite()
+        #self.abstractImage()
         
-        
+    '''
     def clusterImage(self):
-        clusterSize = self.getHeightImage()/30
+        clusterSize = 1 #self.getHeightImage()/30
         self.clusterpoins = []
         h = self.getWidthImage()
-        samples = h*np.int(clusterSize/3)
+        samples = h*2 #h*np.int(clusterSize/3)
         
-        for x in xrange(0, 30):
+        for x in xrange(0, self.getHeightImage()):
             
             tempPoints = []
             nrClusters = 0
@@ -144,8 +149,9 @@ class ImageProcessing():
                             ysum = ysum + cy
                     self.clusterpoins.append([xsum/n, ysum/n])
         self.finalizeImage()
-        
-        
+    '''   
+       
+    '''    
     def abstractImage(self):
         
         self.clusterpoins = []
@@ -153,40 +159,80 @@ class ImageProcessing():
         h = self.getWidthImage()
         
         for x in xrange(0, np.int(w)):
-            whites = []
+            whitesV = []
+            whitesH = []
             self.y = 0
+            
             while self.y < np.int(h):
                 #check if white
                 B, G, R = self.img[self.y][x]
-                if (B == 255 and G == 255 and R == 255):
-                    whites.append([x, self.y])
+                if (B == 255):
+                    whitesV.append([x, self.y])
+                    whitesH.append([x, self.y])
                     #if white, check next ones until there are no more whites and average the point
+                    
+                    #horizontal (x)
+                    for x2 in xrange(1, x-1):
+                        B, G, R = self.img[self.y][x-x2]
+                        if (B == 255):
+                            whitesH.append([x-x2, self.y])
+                        else:
+                            break
+                    for x3 in xrange(1, np.int(h)-x-1):
+                        B, G, R = self.img[self.y][x+x3]
+                        if (B == 255):
+                            whitesH.append([x+x3, self.y])
+                        else:
+                            break
+                    
+                    #vertical (y)
+                    self.yy = np.int(h)-1   
                     for y2 in xrange(self.y+1, np.int(h)):
                         B, G, R = self.img[y2][x]
-                        if (B == 255 and G == 255 and R == 255):
-                            whites.append([x, y2])
+                        if (B == 255):
+                            whitesV.append([x, y2])
                         else:
-                            self.y = y2+1
-                            sumX = 0
-                            sumY = 0
-                            for i in xrange(0, len(whites)):
-                                sumX = sumX + whites[i][1]
-                                sumY = sumY + whites[i][0]
-                            self.clusterpoins.append([sumX/len(whites), sumY/len(whites)])
-                            whites = []
+                            self.yy = y2
                             break
-                self.y = self.y + 1
+                        
+                    #checking for shortest length:
+                    #print(len(whitesV), len(whitesH), x, self.y)
+                    if (len(whitesV) < len(whitesH)):
+                        self.y = self.yy
+                        sumX = 0
+                        sumY = 0
+                        for i in xrange(0, len(whitesV)):
+                            sumX = sumX + whitesV[i][1]
+                            sumY = sumY + whitesV[i][0]
+                        self.clusterpoins.append([sumX/len(whitesV), sumY/len(whitesV)])
+                        
+                    else:
+                        sumX = 0
+                        sumY = 0
+                        for j in xrange(0, len(whitesH)):
+                            sumX = sumX + whitesH[j][1]
+                            sumY = sumY + whitesH[j][0]
+                        self.clusterpoins.append([sumX/len(whitesH), sumY/len(whitesH)])
+                        
+                    #cleaning up the mess this made:
+                     
+                     
+                    whitesH = []
+                    whitesV = []
                     
+                self.y = self.y + 1
+                
         self.finalizeImage()
+    '''
         
-        
+    '''    
     def finalizeImage(self):
         for x in xrange(0, self.getWidthImage()):
             for y in xrange(0, self.getHeightImage()):
                 self.img[x, y] = (0, 0, 0)
         for c in xrange(0, len(self.clusterpoins)):
-            self.img[self.clusterpoins[c][0], self.clusterpoins[c][1]] = (255, 255, 255)
-        
+            self.img[self.clusterpoins[c][1], self.clusterpoins[c][0]] = (255, 255, 255)
+    '''    
         
     def onlyWhite(self):
         for x in xrange(0, self.getWidthImage()):
@@ -194,11 +240,6 @@ class ImageProcessing():
                 B, G, R = self.img[x][y]
                 if (B == 0 and G == 255 and R == 0):
                     self.img[x, y] = (0, 0, 0)
-        
-        
-    def setMaxSpacing(self, m):
-        self.maxSpacing = self.getWidthImage()*m
-        print(self.maxSpacing)
     
     
     def acceptedLumi(self, Y):
@@ -210,7 +251,8 @@ class ImageProcessing():
     
     def acceptedGreen(self, H):
         #H in [0, 255]
-        if (H >= 0.222222 and H <= 0.466667):
+        #if (H >= 0.222222 and H <= 0.466667):
+        if (H >= self.LOWER_BOUND_GREEN and H <= self.UPPER_BOUND_GREEN):
             return True
         return False
     
